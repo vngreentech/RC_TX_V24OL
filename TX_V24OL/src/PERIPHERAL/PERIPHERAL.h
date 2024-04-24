0b552a278f811341d0dd0d20f520b46661c3fab0
NhanNguyen <greentechnology.greengroup@gmail.com>
2024-04-24 22:14:31 +0700

    V24OL
    V24OL.1.4

---
+++ TX_V24OL/src/PERIPHERAL/PERIPHERAL.h
@@ -0,0 +1,11 @@
+
+#ifndef __PERIPHERAL__
+#define __PERIPHERAL__
+
+#include "APPLICATION.h"
+
+void PERIPHERAL_INIT(void);
+void PERIPHERAL_MAIN(void);
+void Check_CountDown(uint8_t StartState);
+
+#endif /* __PERIPHERAL__ */
